612e4a91cedc803401dbcd327c1d4107f18ee82a
Bakhtiyar Orujov <bakhtiyarorujov@gmail.com>
2024-05-20 19:19:09 +0400

    app configuration

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, about_us, contact
 
 urlpatterns = [
-    path('home/', home)
+    path('', home, name='home'),
+    path('about-us/', about_us, name='about_us'),
+    path('contact-us/', contact, name='contact_us')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
    return render(request, 'index.html')
+
+def about_us(request):
+   return render(request, 'about.html')
+
+def contact(request):
+   return render(request, 'contact.html')
